3c0a8f038f414f524a5a2eaf7d119d2292047ed4
Jack Baron <jackmbaron@gmail.com>
2019-01-31 04:38:45 +0000

    Very basic websocket connection

---
+++ AsyncTwitch2/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IllusionPlugin;
+
+namespace AsyncTwitch
+{
+    class Plugin : IPlugin
+    {
+        public static string PluginName = "AsyncTwitch";
+        public static string PluginVersion = "2.0.0";
+
+        public string Name => PluginName;
+        public string Version => PluginVersion;
+
+        public static void Log(object data)
+        {
+            Console.WriteLine($"[{PluginName}] {data}");
+        }
+
+        public static void Debug(object data)
+        {
+#if DEBUG
+            Console.WriteLine($"[{PluginName}] {data}");
+#endif
+        }
+
+        public void OnApplicationStart()
+        {
+            TwitchConnection.OnLoad();
+        }
+
+        public void OnApplicationQuit() { }
+
+        public void OnLevelWasInitialized(int level) { }
+
+        public void OnLevelWasLoaded(int level) { }
+
+        public void OnUpdate() { }
+
+        public void OnFixedUpdate() { }
+    }
+}
---
+++ AsyncTwitch2/TwitchConnection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using WebSocketSharp;
+
+namespace AsyncTwitch
+{
+    public class TwitchConnection : MonoBehaviour
+    {
+        public static TwitchConnection Instance;
+
+        public static WebSocket _ws;
+
+        public static void OnLoad()
+        {
+            if (Instance != null)
+                return;
+
+            new GameObject("AsyncTwitch").AddComponent<TwitchConnection>();
+        }
+
+        public void Awake()
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+            Plugin.Debug("Created AsyncTwitch Gameobject");
+
+            _ws = new WebSocket("wss://irc-ws.chat.twitch.tv");
+
+            _ws.OnOpen += (sender, ev) =>
+            {
+                Plugin.Debug("WebSocket Connection Opened");
+
+                _ws.Send("CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership");
+                _ws.Send("NICK justinfan123456");
+                _ws.Send("PASS bfuyesfhgkesl");
+            };
+
+            _ws.OnClose += (sender, ev) =>
+            {
+                Plugin.Debug($"Socket Closed with reason {ev.Reason}");
+            };
+
+            _ws.OnMessage += _ws_OnMessage;
+            _ws.ConnectAsync();
+        }
+
+        private void _ws_OnMessage(object sender, MessageEventArgs ev)
+        {
+            if (!ev.IsText)
+                return;
+
+            string message = ev.Data.TrimEnd();
+
+#if DEBUG
+            string[] lines = message
+                .Split('\n')
+                .Select((line, i) =>
+                {
+                    string prefix = "Twitch";
+                    string pre = i == 0 ? "Twitch" : new string(' ', prefix.Length);
+
+                    return $"{pre} | {line}";
+                })
+                .ToArray();
+
+            Console.WriteLine(string.Join("\n", lines));
+#endif
+
+            if (message.StartsWith("PING"))
+            {
+                Plugin.Debug("Recieved PING. Sending PONG...");
+                _ws.Send("PONG :tmi.twitch.tv");
+
+                return;
+            }
+        }
+    }
+}
